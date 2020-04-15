4286b4e9cdba5834456d69f64137a94b9f83f537
pyzen5 <pyzen55@gmail.com>
2020-04-15 21:16:50 +0530

    children props

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const person = (props) => {
     // return <p>I'm a {props.name} and I am {Math.floor(Math.random() * 30)} years old!</p>
-    return <p>I'm a {props.name} and I am {props.age} years old!</p>
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>    
+    )
 }
 
 // class-based components - this.props
