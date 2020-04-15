5d4c7b7490654d28b5cae8fb13dcbdc41b1fdc40
pyzen5 <pyzen55@gmail.com>
2020-04-15 22:57:29 +0530

    external style

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     // return <p>I'm a {props.name} and I am {Math.floor(Math.random() * 30)} years old!</p>
     return (
-        <div>
+        <div className="Person">
             <p>I'm a {props.name} and I am {props.age} years old!</p>
             <p onClick={props.click}>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
