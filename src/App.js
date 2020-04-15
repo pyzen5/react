2becb07c8385372a22615c58fecb3bcdf4bcf351
pyzen5 <pyzen55@gmail.com>
2020-04-15 20:57:12 +0530

    component creation variations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>I'm an Hell</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>I'm an Hell</h1>
+  //   </div>
+  // );
+  return React.createElement('div', {className: 'App'}, React.createElement('h1', null,  'I\'m an Hell'))
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>I'm an Hell</h1>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
