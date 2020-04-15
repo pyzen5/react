05cfe4a638a1261a43ae36056a99f4dc505a60b5
pyzen5 <pyzen55@gmail.com>
2020-04-15 22:39:41 +0530

    passing method references

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
     return (
         <div>
             <p>I'm a {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+            <p onClick={props.click}>{props.children}</p>
         </div>    
     )
 }
