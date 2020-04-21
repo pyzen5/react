dc6c0c71ece11a14b7167db9ca4f0147707eed66
pyzen5 <pyzen55@gmail.com>
2020-04-21 15:28:31 +0530

    2-3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutPage from "./components/about/AboutPage";
 import Header from "./components/common/Header";
 import PageNotFound from "./components/PageNotFound";
 import CouresPage from "./components/courses/CoursesPage";
+import ManageCoursePage from "./components/courses/ManageCourse";
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CouresPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
