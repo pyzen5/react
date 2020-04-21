f1d4e081713b7fe911f8d6de868a81d309083f8f
pyzen5 <pyzen55@gmail.com>
2020-04-21 15:23:45 +0530

    2.2

--- src/components/courses/ManageCourse.js
+++ src/components/courses/ManageCourse.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as courseActions from "../../redux/actions/courseActions";
-import * as authorActions from "../../redux/actions/authorActions";
+import { loadCourses } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
 class ManageCoursePage extends React.Component {
@@ -45,8 +45,8 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = {
-  loadCourses: courseActions.loadCourses,
-  loadAuthors: authorActions.loadAuthors,
+  loadCourses,
+  loadAuthors,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
